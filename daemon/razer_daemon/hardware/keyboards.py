f07fd146562e4b4bf10e491023cec1abcfd62399
Terry Cain <terry@terrys-home.co.uk>
2017-01-21 18:46:04 +0000

    Preparing for final changes

--- daemon/razer_daemon/hardware/keyboards.py
+++ daemon/razer_daemon/hardware/keyboards.py
@@ -5,7 +5,7 @@ import re
 
 from razer_daemon.hardware.device_base import RazerDeviceBrightnessSuspend as _RazerDeviceBrightnessSuspend
 from razer_daemon.misc.key_event_management import KeyboardKeyManager as _KeyboardKeyManager, GamepadKeyManager as _GamepadKeyManager, OrbweaverKeyManager as _OrbweaverKeyManager
-from razer_daemon.misc.ripple_effect import RippleManager as _RippleManager
+from razer_daemon.misc.ripple_effect import CustomEffectManager as _CustomEffectManager
 
 
 class _MacroKeyboard(_RazerDeviceBrightnessSuspend):
@@ -192,9 +192,7 @@ class RazerBlackWidowChroma(_MacroKeyboard):
     METHODS = ['get_firmware', 'get_matrix_dims', 'has_matrix', 'get_device_name', 'get_device_type_keyboard', 'get_brightness', 'set_brightness', 'set_wave_effect', 'set_static_effect', 'set_spectrum_effect',
                'set_reactive_effect', 'set_none_effect', 'set_breath_random_effect', 'set_breath_single_effect', 'set_breath_dual_effect',
                'set_custom_effect', 'set_key_row', 'get_game_mode', 'set_game_mode', 'get_macro_mode', 'set_macro_mode',
-               'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro',
-
-               'set_ripple_effect', 'set_ripple_effect_random_colour']
+               'get_macro_effect', 'set_macro_effect', 'get_macros', 'delete_macro', 'add_macro']
 
     RAZER_URLS = {
         "store": "http://www.razerzone.com/gb-en/store/razer-blackwidow-chroma",
@@ -206,7 +204,7 @@ class RazerBlackWidowChroma(_MacroKeyboard):
     def __init__(self, *args, **kwargs):
         super(RazerBlackWidowChroma, self).__init__(*args, **kwargs)
 
-        self.ripple_manager = _RippleManager(self, self._device_number)
+        self.custom_effect_manager = _CustomEffectManager(self._device_number, self.MATRIX_DIMS, self._device_path, self.event_files, parent=self)
 
     def _close(self):
         """
@@ -214,7 +212,7 @@ class RazerBlackWidowChroma(_MacroKeyboard):
         """
         super(RazerBlackWidowChroma, self)._close()
 
-        self.ripple_manager.close()
+        self.custom_effect_manager.close()
 
 
 class RazerBlackWidowChromaTournamentEdition(_MacroKeyboard):
